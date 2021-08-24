f88bd1c78f484ce44892c6f769a3c00a47507f15
David J. Barnes <davidjonbarnes@yahoo.co.uk>
2021-08-24 14:12:21 +0100

    ex6 done

---
+++ Collections/src/main/java/col/Runner.java
@@ -0,0 +1,14 @@
+package col;
+
+
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Collections col = new Collections();
+		System.out.println(col.AddToList());
+		System.out.println(col.ByTen(col.AddToList()));
+	}
+
+}
