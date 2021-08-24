1fba8d2736dce820c151a09232c4366ae869d0d9
David J. Barnes <davidjonbarnes@yahoo.co.uk>
2021-08-24 12:08:42 +0100

    ex5 done

---
+++ Iteration/src/main/java/it/Iteration.java
@@ -0,0 +1,22 @@
+package it;
+
+public class Iteration {
+	Iteration() {}
+	public void PrintNumFromTo(int from, int to) {
+		for(int i=from;i<to;i++) {
+			System.out.println(i);
+		}
+	}
+	public void PrintOneToTenWords() {
+		System.out.println("ONE");
+		System.out.println("TWO");
+		System.out.println("THREE");
+		System.out.println("FOUR");
+		System.out.println("FIVE");
+		System.out.println("SIX");
+		System.out.println("SEVEN");
+		System.out.println("EIGHT");
+		System.out.println("NINE");
+		System.out.println("TEN");
+	}
+}
---
+++ Iteration/src/main/java/it/Runner.java
@@ -0,0 +1,17 @@
+package it;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Iteration ir = new Iteration();
+		ir.PrintNumFromTo(1, 10);
+		System.out.println("------");
+		ir.PrintNumFromTo(100, 999);
+		System.out.println("------");
+		ir.PrintNumFromTo(1, 100);
+		System.out.println("------");
+		ir.PrintNumFromTo(1, 1000);
+	}
+
+}
